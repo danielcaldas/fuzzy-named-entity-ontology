91fe69c2d2beb367341ac9ddcaa364075f562a75
danielcaldas <danielcaldas@sapo.pt>
2016-06-09 15:27:02 +0100

    Examplo básico com módulo GraphViz2

---
+++ Shell/gviz.pl
@@ -0,0 +1,58 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use File::Spec;
+use GraphViz2;
+
+# Global entities configs
+my %entities_configs = (
+  "PESSOA" => (color => "green"),
+  "CIDADE" => (color => "blue"),
+  "DATA" => (color => "red"),
+  "PAIS" => (color => "brown")
+);
+
+my ($graph) = GraphViz2 -> new
+(
+edge   => {color => 'grey'},
+global => {directed => 0},
+graph  => {label => 'Adult', rankdir => 'TB'},
+node   => {shape => 'oval'},
+);
+
+$graph -> add_node(name => 'Carnegie', shape => 'circle');
+$graph -> add_node(name => 'Murrumbeena', shape => 'box', color => 'green');
+$graph -> add_node(name => 'Oakleigh',    color => 'blue');
+
+$graph -> add_edge(from => 'Murrumbeena', to    => 'Carnegie', arrowsize => 2);
+$graph -> add_edge(from => 'Murrumbeena', to    => 'Oakleigh', color => 'brown');
+
+$graph -> push_subgraph
+(
+name  => 'cluster_1',
+graph => {label => 'Child'},
+node  => {color => 'magenta', shape => 'diamond'},
+);
+
+$graph -> add_node(name => 'Chadstone', shape => 'hexagon');
+$graph -> add_node(name => 'Waverley', color => 'orange');
+
+$graph -> add_edge(from => 'Chadstone', to => 'Waverley');
+
+$graph -> pop_subgraph;
+
+$graph -> default_node(color => 'cyan');
+
+$graph -> add_node(name => 'Malvern');
+$graph -> add_node(name => 'Prahran', shape => 'trapezium');
+
+$graph -> add_edge(from => 'Malvern', to => 'Prahran');
+$graph -> add_edge(from => 'Malvern', to => 'Murrumbeena');
+
+my($format)      = shift || 'svg';
+my($output_file) = shift || File::Spec -> catfile('html', "sub.graph.$format");
+
+$graph -> run(format => $format, output_file => $output_file);
+
+exec("eog html/sub.graph.$format");
